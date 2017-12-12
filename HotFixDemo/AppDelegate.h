0f111f025e9754c56b7c1ed8e71cfc0f16d761ed
wzx <wzxlover@gmail.com>
2017-12-12 19:25:05 +0800

    init

---
+++ HotFixDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HotFixDemo
+//
+//  Created by wzx on 2017/12/12.
+//  Copyright © 2017年 topcheer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HotFixDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotFixDemo
+//
+//  Created by wzx on 2017/12/12.
+//  Copyright © 2017年 topcheer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
