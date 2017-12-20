a58cd3829c7556a242dcf35d05dc4b787735ca74
wzx <wzxlover@gmail.com>
2017-12-21 05:38:06 +0800

    change js context

--- js/addbtn.js
+++ js/addbtn.js
@@ -1,6 +1,6 @@
 require("UIButton");
 
-defineClass("HomeViewController", {
+defineClass("JPRootViewController", {
     addButton: function() {
         var btn = UIButton.alloc().initWithFrame({ x: 0, y: 0, width: 80, height: 40 });
         btn.setTitle_forState("这是一个新增的按钮", 0);
@@ -8,7 +8,7 @@ defineClass("HomeViewController", {
         btn.setBackgroundColor(UIColor.whiteColor());
         self.view().addSubview(btn);
     },
-    _setupFunction: function() {
+    _showController: function() {
         self.addButton();
     },
 }, {});
