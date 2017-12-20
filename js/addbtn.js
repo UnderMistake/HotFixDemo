6f0615e5270de5722a5aa04a6da0d4e3a19d74e2
wzx <wzxlover@gmail.com>
2017-12-21 05:38:48 +0800

    fix

--- js/addbtn.js
+++ js/addbtn.js
@@ -2,7 +2,7 @@ require("UIButton");
 
 defineClass("JPRootViewController", {
     addButton: function() {
-        var btn = UIButton.alloc().initWithFrame({ x: 0, y: 0, width: 80, height: 40 });
+        var btn = UIButton.alloc().initWithFrame({ x: 0, y: 0, width: W, height: 50 });
         btn.setTitle_forState("这是一个新增的按钮", 0);
         btn.setTitleColor_forState(UIColor.blackColor(), 1);
         btn.setBackgroundColor(UIColor.whiteColor());
