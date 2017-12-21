4920096aba541f56b6fe685f52d97ad937997eed
wzx <wzxlover@gmail.com>
2017-12-21 13:24:03 +0800

    fix

--- js/addbtn.js
+++ js/addbtn.js
@@ -2,7 +2,7 @@ require('UIButton');
 
 defineClass('JPRootViewController', {
     addButton: function() {
-        var btn = UIButton.alloc().initWithFrame({ x: 0, y: 0, width: 100, height: 50 });
+        var btn = UIButton.alloc().initWithFrame({ x: 0, y: 250, width:100, height: 50 });
         btn.setTitle_forState('thie is a new btn', 0);
         btn.setTitleColor_forState(UIColor.blackColor(), 0);
         btn.setBackgroundColor(UIColor.whiteColor());
